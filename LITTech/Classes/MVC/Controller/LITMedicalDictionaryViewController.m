b47aec1c46f636589c2f5fd9b387e391b73df1a2
Bogdan Poplauschi <bogdyc30@gmail.com>
2013-07-14 21:58:30 +0300

    Med dex with new words

--- LITTech/Classes/MVC/Controller/LITMedicalDictionaryViewController.m
+++ LITTech/Classes/MVC/Controller/LITMedicalDictionaryViewController.m
@@ -32,18 +32,11 @@
         
         self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"First Aid", @"") style:UIBarButtonItemStyleBordered target:self action:@selector(firstAidAction:)];
         
-        _words = @{@"abces" : @"Colectie purulenta constituita, plecand de la un focar local de infectie, pe seama tesuturilor normale. Prin extensie se mai numeste abces sau empiem, colectia purulenta constituita intr-o cavitate seroasa (peritoneu, pleura, meninge). Abcesele se pot dezvolta in oricare punct al organismului. abcesul superficial, accesibil vederii si palparii este amplasat cel mai des la degete (panaritiu) sau pe marginea anusului dar si in gat, pe sezut, subsuoara sau pe partea ventrala. abcesul profund poate fi localizat la nivelul ficatului, rinichiului, creierului, plamanului. Gravitatea sa depinde de localizare: abcesul creierului, fiind asemanator cu tumora, poate provoca hipertensiune intracraniana. Dupa modul lor de constituire si dupa viteza de evolutie, se pot distinge abcesele calde de abcesele reci.",
-                   @"balonare" : @"Simptom caracterizat prin marirea volumului abdomenului provocata de acumularea de gaze in stomac sau in intestin.",
-                   @"calcemie" : @"Nivelul calciului continut in sange. Calcemia este foarte stabila, in jur de 2,5 milimoli pe litru. Aceasta rezulta dintr-un echilibru permanent intre absorbtia intestinala a calciului, fixarea sa in oase sau, din contra, eliberarea sa si eliminarea sa prin urina - hipercalcemie.",
-                   @"dalac" : @"Boala infectioasa contagioasa determinata de infectia cu un bacil gram negativ, Bacillus Anthracis. boala poate fi transmisa omului de catre animale, in principal ovine, cabaline si caprine, vii sau moarte. Contaminarea se face cel mai des in timpul manipularii produselor de ecarisaj, pe cale cutanata sau mucoasa, si uneori prin inhalarea sau ingerarea de spori ai bacteriei. incubatia dureaza doua sau trei zile. Aspectul cel mai caracteristic al bolii carbunelui este o pustula localizata adesea pe fata si care devine repede o tumefactie negricioasa. tratamentul cu antibiotice (penicilina in doza mare), instaurat de urgenta, a trecut aceasta boala in randul celor rare, cu exceptia tarilor in curs de dezvoltare.",
-                   @"ectima" : @"Infectie cutanata caracterizata printr-o ulceratie care survine cel mai des pe membre. Ectima, provocata de o bacterie, streptococul, afecteaza in general subiectii debilitati. Ea se traduce printr-o ulceratie cu coji a pielii. antibioticele (peniciline), luate in urgenta si in doze mari, permit oprirea infectiei. Ingrijirile locale sunt cele ale unei ulceratii (curatire locala, pansamente antiseptice). ",
-                   @"fanera" : @"Organ de protectie caracterizat printr-o keratinizare intensa. parul, dintii, unghiile si perii sunt fanere. keratina, proteina fibrosa si principalul constituent al paturii superficiale a epidermului, este o substanta dura, rezistenta si protectoare.",
-                   @"gastrina" : @"Hormon peptidic secretat de catre celulele endocrine ale antrului gastric (partea inferioara a stomacului) si de catre peretii duodenului si ai jejunului care participa la digestia alimentelor.",
-                   @"halou" : @"Hormon peptidic secretat de catre celulele endocrine ale antrului gastric (partea inferioara a stomacului) si de catre peretii duodenului si ai jejunului care participa la digestia alimentelor.",
-                   @"ictus" : @"Denumire data starilor morbide instalate brusc apopletic, debut brusc al accidentelor vasculare cerebrale insotite de pierderea starii de cunostiinta.",
-                   @"jacket" : @"Proteza de ceramica destinata acoperirii unui dinte deteriorat sau inestetic. Constitutia jachetei asigura unui dinte o mare soliditate si o perfecta asemanare cu dintii invecinati."};
-        
-        _wordsArray = [_words allKeys];
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"MedDex" ofType:@"plist"];
+        _words = [NSDictionary dictionaryWithContentsOfFile:path];
+        _wordsArray = [[_words allKeys] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            return [(NSString *)obj1 compare:(NSString *)obj2];
+        }];
         _filteredWordsArray = [NSArray arrayWithArray:_wordsArray];
     }
     return self;
